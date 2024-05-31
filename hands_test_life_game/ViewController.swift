5ae9184b7aa9091026e7f7b8b45a898aed5e9126
Evgeny <florans1807@mail.ru>
2024-05-31 17:18:47 +0300

    Initial Commit

---
+++ hands_test_life_game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hands_test_life_game
+//
+//  Created by Флоранс on 31.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
